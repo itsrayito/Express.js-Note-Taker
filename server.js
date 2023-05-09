fc3d7b13903de4b1ea68a2fd876d7bae542505a5
itsrayito <rayito.aguirre94@gmail.com>
2023-05-08 20:47:04 -0500

    make it better

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-const express = require("express");
-const apiRoutes = require("./routing/apiRoutes/index");
-const htmlRoutes = require("./routing/htmlRoutes/index");
-const PORT = process.env.port || 3001;
-
+const express = require('express');
+const uuid = require('uuid');
 const app = express();
-
+const PORT = process.env.PORT || 3000;
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
-
-app.use(express.static("public"));
-
-app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`)
-
-);
+app.use(express.static('public'));
+// API routes
+let notes = [];
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+app.post('/api/notes', (req, res) => {
+  const newNote = {
+    id: uuid.v4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  notes.push(newNote);
+  res.json(newNote);
+});
+app.delete('/api/notes/:id', (req, res) => {
+  notes = notes.filter((note) => note.id !== req.params.id);
+  res.json({ msg: 'Note deleted' });
+});
+// HTML routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+// Apply rate limiting to the /notes route
+app.get('/notes', (req, res) => {
+  res.sendFile(__dirname + '/public/notes.html');
+});
+// Start the server
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
